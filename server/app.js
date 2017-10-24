7be2a95b7cb3bf9044118e7569bf5fdf59d1ec64
chandrapothireddy <32874596+chandrapothireddy@users.noreply.github.com>
2017-10-25 00:55:06 +0530

    Create app.js

---
+++ server/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var configs = require('config');
+//...
+var dbConfig = configs.get('example.configs');
+console.log("process.env.NODE_ENV"+process.env.NODE_ENV);
+
+console.log("host "+dbConfig.host);
+
+console.log("port "+dbConfig.port);
+
+console.log("dbName "+dbConfig.db);
+console.log("protocol "+dbConfig.protocol);
+	
+	console.log("secureProtocol "+dbConfig.secureProtocol);
+	console.log("siteUrl "+dbConfig.siteUrl);
+
+if (configs.has('optionalFeature.detail')) {
+  //var detail = config.get('optionalFeature.detail');
+  //...
+}
+var app = express();
+
+
+	
